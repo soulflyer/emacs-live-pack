fae697ab9072ab7474040f06e258464efe454ffa
iain <iain@soulflyer.co.uk>
2017-07-01 22:11:59 +0700

    fixed openwith associations

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 ;;
 ;; Use this file to initiate the pack configuration.
 ;; See README for more information.
-
+;;; Code:
 (live-load-config-file "package.el")
 (live-load-config-file "bindings.el")
 (live-load-config-file "core.el")
@@ -10,8 +10,9 @@
 (live-load-config-file "same-window.el")
 (live-load-config-file "blackbored.el")
 (live-load-config-file "org-config.el")
-(live-load-config-file "mail.el")
+;;(live-load-config-file "mail.el")
 (live-load-config-file "grep.el")
 (live-load-config-file "rocks.el")
 (live-load-config-file "refactor.el")
 (color-theme-blackbored)
+;;; init.el ends here
